af792d01df09dabc5290bcbb4e5707ed19a0e763
David Garcia Facio <david.fascio@gmail.com>
2014-11-24 13:21:53 -0600

    reorden

---
+++ atmega8535/DivisasDisplayMain.c
@@ -0,0 +1,34 @@
+/*
+ * File: DivisasDisplayMain.h
+ * Author: fascio
+ * 
+ * Created on November 10th, 2014 12:00:00
+ */ 
+ 
+//**********************************************************************
+// Includes
+//**********************************************************************
+#include "ScreenDisplayDevice.h"
+#include "ScreenDisplayProtocol.h"
+
+//**********************************************************************
+// API Fucntions
+//**********************************************************************
+void main(void)
+{
+	//! SETUP
+	bsp_setup();    
+	ScreenDisplayProtocol_Setup();	
+	
+	//! AFTER SETUP
+	ScreenDisplayDevice_Setup();
+
+	//! LOOP
+	while(TRUE){
+		
+		ScreenDisplayProtocol_WaitDataPacketCheck();
+		ScreenDisplayProtocol_ProcessingDataPacketArrived();	
+        //SEND_DEMO();
+		bsp_delay_ms(3000);	
+	}
+}
---
+++ atmega8535/psp.h
@@ -0,0 +1,58 @@
+/*
+ * File: psp.h
+ * Author: fascio
+ * 
+ * Created on November 20th, 2014, 09:34
+ */ 
+ 
+#ifndef __PSP_H__
+#define __PSP_H__
+
+//**********************************************************************
+// Includes
+//**********************************************************************
+#ifdef __ATMEGA_1284P__
+#include <mega1284p.h>
+#endif /* #ifdef __ATMEGA_1284P__ */
+
+
+//**********************************************************************
+// Vartypes
+//**********************************************************************
+typedef char 	boolean;
+
+//**********************************************************************
+// Defines
+//**********************************************************************
+#define TRUE  												((boolean) 1)
+#define FALSE  												((boolean) 0)
+
+//**********************************************************************
+// Util Prototype Functions
+//**********************************************************************
+int meminvrtncpy(char * BufferDestination, char * BufferSource, long SizeOfBuffers);
+
+
+//**********************************************************************
+// Util Functions
+//**********************************************************************
+int meminvrtncpy(char * BufferDestination, char * BufferSource, long SizeOfBuffers)
+{
+	char * BufferSourcePointer;
+	if((BufferSource == NULL) || (BufferDestination == NULL) || (SizeOfBuffers <= 0))
+	return -1;
+	
+	
+	BufferSourcePointer = BufferSource + SizeOfBuffers - 1;
+	
+	while(BufferSourcePointer >= BufferSource)
+	{
+	    *BufferDestination = *BufferSourcePointer;
+		
+		BufferSourcePointer--;
+		BufferDestination++;
+	}
+	return 1;
+}
+
+#endif /* __PSP_H__ */
